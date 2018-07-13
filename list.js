395b288c4317c4535cbc3a2668b1a890f6b28309
Quanluis <quanluis@protonmail.com>
2018-07-12 22:17:08 -0400

    initial setup of list creator

---
+++ list.js
@@ -0,0 +1,10 @@
+const listCreator  = document.querySelector('input');
+const addItem = document.querySelector('button');
+
+listCreator.addEventListener('click', () => {
+
+})
+
+addItem.addEventListener('click', () => {
+   console.log('hello')
+})
